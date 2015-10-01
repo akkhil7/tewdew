f5d18fa4b1849b79ff31f9c813314ee7c9a10b13
Akhil Ramesh <akkhil@outlook.com>
2015-10-02 01:18:59 +0530

    configured to heroku

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -8,10 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Todoo
   class Application < Rails::Application
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+   config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :put, :post, :delete, :patch, :options]
+        resource '*', :headers => :any, :methods => [:get, :patch, :put, :delete, :post, :options]
       end
     end
   end
